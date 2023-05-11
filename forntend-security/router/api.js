1778167ef643b5a425062f61a3c0d3d4e4b09b9b
渡邉将太 <shotawebcoding@gmail.com>
2023-05-11 23:41:40 +0900

    Update api.js

--- forntend-security/router/api.js
+++ forntend-security/router/api.js
@@ -2,9 +2,32 @@
 const express = require("express");
 const router = express.Router();
 
+router.use(express.json());
+
 //GETメソッドでリクエストを受け取った時の処理を定義
 router.get("/", (req, res) => {
-    res.send({ message: "Hello" });
+    res.setHeader("X-Timestamp", Date.now()); //現在の時刻をデータとして受け取る
+    let message = req.query.message; //クエリ文字列を受け取る
+    const lang = req.headers["x-lang"];
+
+    if (message === "") {
+        res.status(400);
+
+        if (lang === "en") {
+            message = "message is empty";
+        } else {
+            message = "messageの値は空です";
+        }
+
+        message = "messageの値が空です。";
+    }
+    res.send({ message });
+});
+
+router.post("/", (req, res) => {
+    const body = req.body; //req.bodyにはリクエストボディが格納されている
+    console.log(body);
+    res.end();
 });
 
 //他ファイルでも読み込めるようにエクスポート
